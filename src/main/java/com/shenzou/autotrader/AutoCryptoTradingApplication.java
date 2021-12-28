8c9dcec6cba13055a0a96a9c31f50c3d3873c76a
Alexandre Shenouda <contact@alexshenouda.fr>
2021-12-28 14:40:38 +0100

    Initial commit

---
+++ src/main/java/com/shenzou/autotrader/AutoCryptoTradingApplication.java
@@ -0,0 +1,13 @@
+package com.shenzou.autotrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCryptoTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCryptoTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shenzou/autotrader/AutoCryptoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenzou.autotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCryptoTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
